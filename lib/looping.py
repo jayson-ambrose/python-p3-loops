18ec37a73bd4aaeb7eae2b675f4b596856355a9e
jayson ambrose <jayson.ambrose.87@gmail.com>
2023-03-13 22:19:49 -0600

    lab complete

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,37 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
+
+    i = 10
+
+    while i >=  0:        
+        
+        if(i == 0):
+            print("Happy New Year!")
+            i= i - 1
+        else:
+            print(i)
+            i = i - 1
     pass
 
 def square_integers(int_list):
-    # code goes here!
-    pass
+    int_list = [int * int for int in int_list]
+    return int_list
 
 def fizzbuzz():
-    # code goes here!
+
+    x = 1
+
+    for x in range(1, 101):
+
+        if(x % 3 == 0 and x % 5 == 0):
+            print("FizzBuzz")
+        elif(x % 3 ==  0):
+            print("Fizz")
+        elif(x % 5 == 0):
+            print("Buzz")
+        else:
+            print(f"{x}")
     pass
+
+happy_new_year()
